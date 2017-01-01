83360beb59a359cd2734c8f98aa643886a0d1c5c
daidodo <daidodo@gmail.com>
2017-01-02 05:22:22 +0800

    update doc;

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['marine_20library',['Marine Library',['../index.html',1,'']]],
   ['manip',['Manip',['../namespaceManip.html',1,'']]],
   ['max_5fsize',['max_size',['../classCCharBuffer.html#a9da9367e03ee90bb7c350e7e19791837',1,'CCharBuffer']]],
   ['mode',['mode',['../classCCharsetConvert.html#a269b04d77ff9cec473f14b581d0c2cff',1,'CCharsetConvert']]],
---
+++ docs/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['marine_20library',['Marine Library',['../index.html',1,'']]]
+];
