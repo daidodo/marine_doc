87de408114f2a20abc5e1bca19d2246c74160778
daidodo <daidodo@gmail.com>
2017-01-09 12:54:42 +0800

    update docs;

--- docs/search/functions_11.js
+++ docs/search/functions_11.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['uninit',['uninit',['../classCAtomicHashTable.html#af51ad9a839f4585659a6cbbd853a79eb',1,'CAtomicHashTable']]],
-  ['unlink',['Unlink',['../classCFile.html#ac6a5b1a24d198fc968e569fc1c619830',1,'CFile']]],
+  ['unlink',['unlink',['../classCFile.html#a8cc6da8611dd2ebf922e29305c140cff',1,'CFile::unlink()'],['../classCPosixShmFile.html#aba769ec0b0f4787e00488dc6d521ef42',1,'CPosixShmFile::unlink()'],['../classCFile.html#ac6a5b1a24d198fc968e569fc1c619830',1,'CFile::Unlink()']]],
   ['update',['update',['../classCAtomicHashTable.html#aae57ecbd78dbee1819d6f72eb8bfd373',1,'CAtomicHashTable::update(const key_type &amp;key, const char *value, size_t len)'],['../classCAtomicHashTable.html#a801c0fc4da0c553ed0efed5a00abc67b',1,'CAtomicHashTable::update(const key_type &amp;key, const std::string &amp;value)']]],
   ['updatetime',['updateTime',['../classCAtomicHashTable.html#aa5c4031f2ab0f4cbd2f5361c447b569a',1,'CAtomicHashTable']]],
   ['upgradetime',['upgradeTime',['../classCAtomicHashTable.html#a77a8e143d2b372d16f950ce9642826b1',1,'CAtomicHashTable']]]
